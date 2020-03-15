237ef3478cc6d31ca8771721b252f1ea6ac1ec43
Keaton Neville <keaton.185@gmail.com>
2020-03-15 15:12:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
               Originally from a technical consulting background, now focussed on 
               building innovative digital solutions.
               <br />
-              I'm currently based in Toronto, Canada and am open for opportunities in Canada.
+              I'm currently based in Toronto, Canada.
             </p>
           </div>
         </div>
